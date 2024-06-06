17b8a69d96d6c05ba1e4235d5b29e4eea7810fdd
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-06-06 11:59:58 +0100

    add buttons and grab them in the js

---
+++ script.js
@@ -0,0 +1,19 @@
+let boomSound = new Audio("./sounds/boom.wav");
+let clapSound = new Audio("./sounds/clap.wav");
+let hihatSound = new Audio("./sounds/hihat.wav");
+let kickSound = new Audio("./sounds/kick.wav");
+let openhatSound = new Audio("./sounds/openhat.wav");
+let rideSound = new Audio("./sounds/ride.wav");
+let snareSound = new Audio("./sounds/snare.wav");
+let tinkSound = new Audio("./sounds/tink.wav");
+let tomSound = new Audio("./sounds/tom.wav");
+
+const boomBtn = document.getElementById("boomBtn");
+const clapBtn = document.getElementById("clapBtn");
+const hihatBtn = document.getElementById("hihatBtn");
+const kickBtn = document.getElementById("kickBtn");
+const openhatBtn = document.getElementById("openhatBtn");
+const rideBtn = document.getElementById("rideBtn");
+const snareBtn = document.getElementById("snareBtn");
+const tinkBtn = document.getElementById("tinkBtn");
+const tomBtn = document.getElementById("tomBtn");
