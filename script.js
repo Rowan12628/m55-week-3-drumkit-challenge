eb52da0fb76c3fc63d76695588800770f4940dca
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-06-06 12:09:40 +0100

    add event listeners to the buttons so they play sound

--- script.js
+++ script.js
@@ -17,3 +17,39 @@ const rideBtn = document.getElementById("rideBtn");
 const snareBtn = document.getElementById("snareBtn");
 const tinkBtn = document.getElementById("tinkBtn");
 const tomBtn = document.getElementById("tomBtn");
+
+boomBtn.addEventListener("click", () => {
+  boomSound.play();
+});
+
+clapBtn.addEventListener("click", () => {
+  clapSound.play();
+});
+
+hihatBtn.addEventListener("click", () => {
+  hihatSound.play();
+});
+
+kickBtn.addEventListener("click", () => {
+  kickSound.play();
+});
+
+openhatBtn.addEventListener("click", () => {
+  openhatSound.play();
+});
+
+rideBtn.addEventListener("click", () => {
+  rideSound.play();
+});
+
+snareBtn.addEventListener("click", () => {
+  snareSound.play();
+});
+
+tinkBtn.addEventListener("click", () => {
+  tinkSound.play();
+});
+
+tomBtn.addEventListener("click", () => {
+  tomSound.play();
+});
